8ad9503c6669d5c67c1f60ffbef5f6e541659c97
zhou-dong <82224165@qq.com>
2015-03-07 23:26:12 -0600

    normalization

--- normalization-keywords.py
+++ normalization-keywords.py
@@ -1,16 +1,47 @@
-import nltk
-from nltk.probability import ConditionalFreqDist
-from nltk.tokenize import word_tokenize
+# -*- coding: utf-8 -*-
 
-# nltk.download()
+from pymongo import MongoClient
+from nltk.stem.lancaster import LancasterStemmer
 
-sent = "the the the dog dog some other words that we do not care about mining mined"
-cfdist = ConditionalFreqDist()
+st = LancasterStemmer()
 
-#for word in word_tokenize(sent):
-#    condition = len(word)
-#    cfdist[condition][word] += 1
+client = MongoClient()
+db = client['linkedin']
+collection = db['cleaned-skills']
 
+def calculateCursorTime(limit):
+    totalCount =  collection.count()
+    result = totalCount / limit
+    if totalCount % limit == 0:
+        return result 
+    else :
+        return result + 1
 
-for word in word_tokenize(sent):
-    print word
+def isUpdate(obj, keyword):
+    try:
+        if(None==obj["stem"]):
+            print keyword
+    except:
+        print keyword
+
+def insertStemWord(time):
+    begin = time * limit
+    cursor = collection.find().skip(begin).limit(limit)
+    for obj in cursor :
+        keyword = obj["_id"]
+        isUpdate(obj, keyword)
+        try:
+            stem_keyword = st.stem(keyword)
+        except:
+            print "stem exception: ",  keyword
+            stem_keyword = keyword
+        obj["stem"] = stem_keyword
+        collection.update({'_id': keyword}, obj, True)
+    print (begin + limit), "finished"
+
+limit = 1000
+
+for x in range(0, calculateCursorTime(limit)):
+    insertStemWord(x)
+
+client.close()
