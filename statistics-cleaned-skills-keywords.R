5e00cb40f6ce5882e7607db78ad2a4cb26bb6098
zhou-dong <82224165@qq.com>
2015-03-09 22:44:47 -0500

    update

--- statistics-cleaned-skills-keywords.R
+++ statistics-cleaned-skills-keywords.R
@@ -46,5 +46,5 @@ counts <- counts[counts > 0]
 # print(sum(counts))
 
 count_labels <- round(counts/sum(counts) * 100, 1)
-# pie(counts, main="Cleaned Keywords", col=rainbow(length(cars)), labels=count_labels)
-pie(counts, main="Cleaned Keywords", col=rainbow(length(cars)))
+pie(counts, main="Cleaned Keywords", col=rainbow(length(cars)), labels=count_labels)
+# pie(counts, main="Cleaned Keywords", col=rainbow(length(cars)))
