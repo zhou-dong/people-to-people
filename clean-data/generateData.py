4bd0febb6c05dd10d2ca905c2cdf3aa68c3cb63e
zhou-dong <82224165@qq.com>
2015-03-11 11:11:41 -0500

    update

--- clean-data/generateData.py
+++ clean-data/generateData.py
@@ -1,11 +1,9 @@
+import json
+
 from pymongo import MongoClient
 from cleanLine import CleanLine
 
 clean_line = CleanLine()
-
-client = MongoClient()
-db = client['linkedin']
-ns_people = db["employee"]
 
 def cursor_time(total, limit):
     result = total / limit
@@ -61,25 +59,88 @@ def create_industry(obj):
     global count_no_industry
     try:
         industry = obj["industry"]
-        print industry
         return clean_line.execute(industry)
     except:
         count_no_industry += 1
 
-begin = 0 
-limit = 100
-cursor = ns_people.find().skip(begin).limit(limit)
+count_no_first_name = 0
+def getFirstName(obj):
+    global count_no_first_name
+    try:
+        return obj["first-name"]
+    except:
+        count_no_first_name += 1
 
-for obj in cursor:
-    user_id = obj["_id"]
- #   print create_skills(obj)
- #   print create_educations(obj)
- #   print create_positions(obj)
-    print create_industry(obj)
 
-print count_no_skill
-print count_no_edu
-print count_no_position
-print count_no_industry
+count_no_last_name = 0 
+def getLastName(obj):
+    global count_no_last_name
+    try:
+        return obj["last-name"]
+    except:
+        count_no_last_name += 1
 
-client.close()
+count_no_summary = 0
+def createSummary(obj):
+    global count_no_summary
+    try:
+        summary = obj["summary"]
+        return clean_line.execute(summary)
+    except:
+        count_no_summary += 1
+
+def batch_insert_peope(time, limit): 
+    peoples = []
+    begin = time * limit 
+    cursor = ns_employ.find().skip(begin).limit(limit)
+    for obj in cursor:
+        user_id = obj["_id"]
+        skills = create_skills(obj)
+        edus = create_educations(obj)
+        positions = create_positions(obj)
+        industry = create_industry(obj)
+        firstname = getFirstName(obj),
+        lastname = getLastName(obj)
+        summary = createSummary(obj)
+        people = {"user_id": user_id, "skills": skills, 
+                 "educations": edus, "positions":positions,
+                 "industry":industry, "firstname":firstname[0],
+                 "lastname":lastname,"summary":summary}
+        peoples.append(people)
+    ns_people.insert(peoples)
+    print "finish: ", (begin + limit)
+
+client = MongoClient()
+db = client['linkedin']
+ns_people = db["people"]
+ns_employ = db["employee"]
+
+total = 0
+def insert_people():
+    global total
+    print "begin to insert"
+    limit = 10
+    try:
+        total = ns_employ.count()
+        times = cursor_time(total, limit)
+        for time in range(0, times):
+            if time==1:
+                break
+            batch_insert_peope(time, limit)
+    except Exception as e:
+        print str(e)
+    finally:
+        client.close()
+        print "client closed"
+
+insert_people()
+
+print total
+total = float(total)
+print "no skills: ", count_no_skill, count_no_skill / total
+print "no educations: ", count_no_edu, count_no_edu / total 
+print "no positions: ", count_no_position, count_no_position / total
+print "no industry: ", count_no_industry, count_no_industry / total
+print "no firstname: ", count_no_first_name, count_no_first_name / total
+print "no lastname: ", count_no_last_name, count_no_last_name / total
+print "no summary: ", count_no_summary, count_no_summary / total
